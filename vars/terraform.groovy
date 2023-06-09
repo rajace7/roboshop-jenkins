25b02e60806a67cf4e138b32bd123d42d5bf55e4
2rpadala <rajvja520002@gmail.com>
2023-06-09 13:53:07 +0530

    seed-job

---
+++ vars/terraform.groovy
@@ -0,0 +1,43 @@
+def call() {
+    pipeline {
+
+        agent {
+            node {
+                label 'workstation'
+            }
+        }
+
+        options {
+            ansiColor('xterm')
+        }
+
+        parameters {
+            choice(name: 'env', choices: ['dev', 'prod'], description: 'Pick environment')
+        }
+
+        stages {
+
+            stage('Terraform INIT') {
+                steps {
+                    sh 'terraform init -backend-config=env-${env}/state.tfvars'
+                }
+            }
+
+            stage('Terraform Apply') {
+                steps {
+                    sh 'terraform apply -auto-approve -var-file=env-${env}/main.tfvars'
+                }
+            }
+
+        }
+
+        post {
+            always {
+                cleanWs()
+            }
+        }
+
+    }
+
+
+}
