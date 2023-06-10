d8377ff192fd88c29d5bf88fecb840a59f14a7c8
2rpadala <rajvja520002@gmail.com>
2023-06-10 12:16:14 +0530

    mjob

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -16,13 +16,14 @@ def call() {
             stage('Code Quality') {
                 steps {
                     sh 'echo Code Quality'
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.235:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
+                    //sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.83.235:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
                 }
             }
 
             stage('Unit Test Cases') {
                 steps {
-                    sh 'echo Unit tests'
+                    //sh 'echo Unit tests'
+                    sh 'npm test'
 
                 }
             }
