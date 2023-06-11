66b627c3f3cc54dc999b6162881024879e8065ca
2rpadala <rajvja520002@gmail.com>
2023-06-11 23:35:11 +0530

    mjob

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -54,6 +54,7 @@ def call() {
                     sh 'echo $TAG_NAME >VERSION'
                     sh 'zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION'
                     sh 'curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.95.142:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+
                 }
             }
 
