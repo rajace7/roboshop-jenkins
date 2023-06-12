2dabca409be466220317b420ee39ddf55157ec36
2rpadala <rajvja520002@gmail.com>
2023-06-12 17:16:28 +0530

    mjob

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -53,7 +53,7 @@ def call() {
                 steps {
 
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip *'
                     sh 'zip -d ${component}-${TAG_NAME}.zip Jenkinsfile'
                     sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.95.142:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
