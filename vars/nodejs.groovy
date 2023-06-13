55ee026e075fb820647f88c77722ba9a0a987d4c
2rpadala <rajvja520002@gmail.com>
2023-06-13 12:02:59 +0530

    mjob

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -53,7 +53,7 @@ def call() {
                 steps {
                     sh 'npm install'
                     sh 'echo $TAG_NAME >VERSION'
-                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION'
+                    sh 'zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${schema_dir}'
                     sh 'curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${component}-${TAG_NAME}.zip http://172.31.95.142:8081/repository/${component}/${component}-${TAG_NAME}.zip'
                 }
             }
